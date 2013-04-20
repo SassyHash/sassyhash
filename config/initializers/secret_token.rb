3497f12feb4b3c35273afb92d6e1be032b1756a8
Brittany Yoon <binna.yoon@gmail.com>
2013-04-20 16:28:26 -0400

    Started personal website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SassyHash::Application.config.secret_token = '48707002a5c62e7fc2108c8dfddee8e301b541b2446cd4feee3d88b639cf5302a45b6b7526154df58ec0d7ecfaa4328cdb5c0e04b158aeb136f75dbe5ff2b7db'
